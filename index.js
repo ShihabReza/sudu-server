7c95628ec0e5df662d4c7678568bc44e6cbac92f
ShihabReza <shihabreza457@gmail.com>
2022-07-02 00:47:38 +0600

    first commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
-  console.log('DB Connection')
+  console.log('DB Connectiond')
   // perform actions on the collection object
   // client.close();
 });
